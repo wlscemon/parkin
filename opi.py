e398a381662d19b70e0b30a5434d00cb6aacda92
danica <59591384+wlscemon@users.noreply.github.com>
2021-03-05 12:39:29 +0800

    Update opi.py

--- opi.py
+++ opi.py
@@ -5,3 +5,8 @@ import numpy as np
 import pandas as pd
 
 st.title('My first app')
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
